6822a8b589af6a319cd49570827db8abf8caea27
Chen-Wei,Li <89914044+JosephLi0419@users.noreply.github.com>
2024-05-17 16:33:30 +0800

    Create token_counter.py

---
+++ other skills/count tokens/token_counter.py
@@ -0,0 +1,28 @@
+from sentencepiece import SentencePieceProcessor
+import json
+
+# Initialize SentencePieceProcessor with your model file
+sp = SentencePieceProcessor(model_file="/home/yabilab/Desktop/tokenizer.model") #MediaTek-Research/Breeze-7B-Instruct-v1_0 #Taiwan-LLM-7B-v2.1-chat
+
+def tokenize(input_text):
+    # Tokenize input_text using SentencePiece
+    tokens = sp.EncodeAsIds(input_text) #Encoder: 对应预处理时的tokenization操作，把句子转为对应的subword或id。
+    return len(tokens)
+
+# Path to your JSON file
+json_file_path = "/home/yabilab/Desktop/LLaMA-Factory-main/data/AI_conversation.json" #你想要計算的對話檔
+
+
+# Load JSON data from file
+with open(json_file_path, "r", encoding="utf-8") as file:
+    json_data = json.load(file)
+
+total_token_count = 0
+
+# Iterate over conversations and accumulate token counts
+for conversation in json_data:
+    for conv in conversation["conversations"]:
+        if "value" in conv:
+            total_token_count += tokenize(conv["value"])
+
+print("Total token count:", total_token_count)
